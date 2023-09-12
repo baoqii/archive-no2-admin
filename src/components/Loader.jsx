85a1a312174103f22968b043cad75f75c860cbca
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-12 10:35:06 +0800

    Add Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,22 @@
+import { ThreeDots } from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <section className="relative w-[700px] ml-[400px] ">
+      <div className="flex flex-col justify-center items-center h-[600px] gap-2">
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#9ba5cb"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Loader;
